2f4c694e36117e5f8c69df5a55b6575ee8e1cebd
Zachary Sousa <75133905+zach-sousa-dev@users.noreply.github.com>
2024-04-09 15:58:06 +0000

    ready for submit

--- panelToggle.js
+++ panelToggle.js
@@ -1,7 +1,13 @@
 var rawElements = document.querySelectorAll(".item");
 
-for(let i = 0; i < rawElements.length; i++) {
+for (let i = 0; i < rawElements.length; i++) {
 	rawElements[i].querySelector(".item-title").addEventListener("click", function(event) {
 		rawElements[i].querySelector(".item-content").classList.toggle("hidden")
+		console.log(rawElements[i].querySelector(".item-content"));
+		if (rawElements[i].querySelector(".item-content").className.includes("hidden")) {
+			rawElements[i].querySelector(".item-title").querySelector(".arrow").setAttribute('src', 'images/down.svg');
+		} else {
+			rawElements[i].querySelector(".item-title").querySelector(".arrow").setAttribute('src', 'images/left.svg');
+		}
 	});
 }
