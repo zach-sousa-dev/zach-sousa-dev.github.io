0e1b7830af70c08989e6b82af0459d01b51bd367
zach-sousa-dev <zacharysousa@outlook.com>
2026-09-19 22:41:44 -0400

    added animations to the SocialButtons

--- src/lib/components/SocialIcon.svelte
+++ src/lib/components/SocialIcon.svelte
@@ -1,16 +1,38 @@
 <script>
+    import { Tween } from "svelte/motion"; 
+    import { elasticOut } from 'svelte/easing'
+
     let { img, link } = $props();
+
+    let progress = new Tween(1, {
+        duration: 600,
+        easing: elasticOut
+    });
+    
 </script>
 
 <div class="
     mr-5
     mt-5
 ">
-        <a href={link} target="_blank" class="
+        <a href={link} target="_blank" 
+        onmouseenter={() => {
+            progress.target = 1.2
+        }}
+        onmouseleave={() => {
+            progress.target = 1
+        }}
+        style='transform: scale({progress.current})'
+        class="
             rounded-full
             bg-passionate-red
             p-4
             flex
+            shadow-md/50
+            shadow-black
+            border-highlight-red
+            border-t
+            border-l
         ">
             <img src={img} alt="icon button" class="
                 h-12
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -31,25 +31,4 @@
         <SocialIcon img={mail} link="mailto:zacharysousa@outlook.com?subject=I found your website!" />
     </BusinessCard>
 
-    <BusinessCard 
-        topMargin="mt-0" 
-        img="https://avatars.githubusercontent.com/u/75133905?v=4"
-        header="Zachary Sousa"
-        body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mollis placerat mauris vel pharetra. Ut lobortis tortor leo, quis elementum nunc scelerisque iaculis. Duis eget turpis non nunc pharetra sagittis eu in sapien. Nam vestibulum ante sed metus facilisis, sed lobortis quam varius. Integer eget faucibus erat. Vestibulum vitae sodales arcu. Donec vel eleifend dui. Proin facilisis metus sit amet dui semper facilisis. Proin bibendum blandit metus vitae fringilla. Nulla id scelerisque magna. "
-    >
-        <SocialIcon img={linkedIn} link="https://www.linkedin.com/in/zach-sousa/" />
-        <SocialIcon img={gitHub} link="https://github.com/zach-sousa-dev" />
-        <SocialIcon img={mail} link="mailto:zacharysousa@outlook.com?subject=I found your website!" />
-    </BusinessCard>
-
-    <BusinessCard 
-        topMargin="mt-0" 
-        img="https://avatars.githubusercontent.com/u/75133905?v=4"
-        header="Zachary Sousa"
-        body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mollis placerat mauris vel pharetra. Ut lobortis tortor leo, quis elementum nunc scelerisque iaculis. Duis eget turpis non nunc pharetra sagittis eu in sapien. Nam vestibulum ante sed metus facilisis, sed lobortis quam varius. Integer eget faucibus erat. Vestibulum vitae sodales arcu. Donec vel eleifend dui. Proin facilisis metus sit amet dui semper facilisis. Proin bibendum blandit metus vitae fringilla. Nulla id scelerisque magna. "
-    >
-        <SocialIcon img={linkedIn} link="https://www.linkedin.com/in/zach-sousa/" />
-        <SocialIcon img={gitHub} link="https://github.com/zach-sousa-dev" />
-        <SocialIcon img={mail} link="mailto:zacharysousa@outlook.com?subject=I found your website!" />
-    </BusinessCard>
 </div>
