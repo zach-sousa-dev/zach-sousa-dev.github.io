33977167ce50f5fdbbd8278ee06ee921e09d9e2e
zach-sousa-dev <zacharysousa@outlook.com>
2026-09-21 18:38:37 -0400

    added scroll animation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
     import Navbar from "$lib/components/Navbar.svelte";
     import BusinessCard from "$lib/components/BusinessCard.svelte";
+    import Header from "$lib/components/Header.svelte";
 
     import SocialIcon from "$lib/components/SocialIcon.svelte";
     import linkedIn from "$lib/assets/in.svg";
@@ -8,13 +9,22 @@
     import mail from "$lib/assets/mail.svg";
 
     import './layout.css';
-    import Header from "$lib/components/Header.svelte";
+
+    import { scrollTo, scrollRef, scrollTop, setGlobalOptions } from 'svelte-scrolling'
+
+    import { expoOut } from 'svelte/easing'
+
+    // page scroll options
+    setGlobalOptions({
+        duration: 800,
+        easing: expoOut
+    })
 </script>
 
 <Navbar>
-    <a href="/">About</a>
-    <a href="/">Services</a>
-    <a href="/">Portfolio</a>
+    <a href="\" use:scrollTo={'about'}>About</a>
+    <a href="\" use:scrollTo={'services'}>Services</a>
+    <a href="\" use:scrollTo={'portfolio'}>Portfolio</a>
 </Navbar>
 
 <div 
@@ -23,15 +33,47 @@
         flex-col
     "
 >
-    <BusinessCard 
-        topMargin="mt-90" 
-        img="https://avatars.githubusercontent.com/u/75133905?v=4"
-        header="Zachary Sousa"
-        body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mollis placerat mauris vel pharetra. Ut lobortis tortor leo, quis elementum nunc scelerisque iaculis. Duis eget turpis non nunc pharetra sagittis eu in sapien. Nam vestibulum ante sed metus facilisis, sed lobortis quam varius. Integer eget faucibus erat. Vestibulum vitae sodales arcu. Donec vel eleifend dui. Proin facilisis metus sit amet dui semper facilisis. Proin bibendum blandit metus vitae fringilla. Nulla id scelerisque magna. "
-    >
-        <SocialIcon img={linkedIn} link="https://www.linkedin.com/in/zach-sousa/" />
-        <SocialIcon img={gitHub} link="https://github.com/zach-sousa-dev" />
-        <SocialIcon img={mail} link="mailto:zacharysousa@outlook.com?subject=I found your website!" />
-    </BusinessCard>
+
+    <section use:scrollRef={'about'}>
+        <BusinessCard 
+            topMargin="mt-90" 
+            img="https://avatars.githubusercontent.com/u/75133905?v=4"
+            header="Zachary Sousa"
+            body="I've just recently completed the P-TECH program that I was fortunately a part of during my experience at SNP Secondary School (formerly SNP STEAM Academy) and Mohawk College. I've since transferred to another Software Development program at Mohawk!"
+        >
+            <SocialIcon img={linkedIn} link="https://www.linkedin.com/in/zach-sousa/" />
+            <SocialIcon img={gitHub} link="https://github.com/zach-sousa-dev" />
+            <SocialIcon img={mail} link="mailto:zacharysousa@outlook.com?subject=I found your website!" />
+        </BusinessCard>
+
+        <br><br><br><br><br><br><br><br><br><br>
+        <br><br><br><br><br><br><br><br><br><br>
+        <br><br><br><br><br><br><br><br><br><br>
+        <br><br><br><br><br><br><br><br><br><br>
+    </section>
+
+
+    <section use:scrollRef={'services'}>
+
+        <Header>Services</Header>
+        <br><br><br><br><br><br><br><br><br><br>
+        <br><br><br><br><br><br><br><br><br><br>
+        <br><br><br><br><br><br><br><br><br><br>
+
+    </section>
+
+    <section use:scrollRef={'portfolio'}>
+
+        <Header>Portfolio</Header>
+        <br><br><br><br><br><br><br><br><br><br>
+        <br><br><br><br><br><br><br><br><br><br>
+        <br><br><br><br><br><br><br><br><br><br>
+
+    </section>
+
+    <br><br><br><br><br><br><br><br><br><br>
+    <br><br><br><br><br><br><br><br><br><br>
+    <br><br><br><br><br><br><br><br><br><br>
+
 
 </div>
