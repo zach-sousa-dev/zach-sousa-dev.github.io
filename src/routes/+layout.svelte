7d5418054ed65942bd41005568bd6edf0e92cf42
zach-sousa-dev <zacharysousa@outlook.com>
2026-09-22 21:29:49 -0400

    added favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/head.svg';
 
 	let { children } = $props();
 </script>
